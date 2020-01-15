a9ce5f977bb346e3ae9baba4c10c3bcb0f64beb1
pengdake <19921207pq@gmail.com>
2020-01-15 19:21:31 +0800

    update response

--- api-server/api.py
+++ api-server/api.py
@@ -75,12 +75,9 @@ def upload_file():
         upload_path = os.path.join(basepath, filename)
         f.save(upload_path)
         res = Recognize(upload_path, model_path, model_type)
-        result = {}
-        result = {"type": "text"}
-        if not res:
-            result['data'] = ''
-            return make_response(jsonify(result), 200)
-        result['data'] = res
+        result = {"type": "text", "data": ""}
+        if res:
+            result['data'] = res
         os.remove(upload_path)
         return make_response(jsonify(result), 200)
     except Exception as err:
